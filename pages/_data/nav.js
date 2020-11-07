a690331bda46a505b8a3c22a5cbcdd51005c8aa1
dommartin25 <loic.espern@gmail.com>
2020-11-07 12:38:43 +0100

    optimize menu

--- pages/_data/nav.js
+++ pages/_data/nav.js
@@ -1,10 +1,10 @@
 exports.data = [
   {label: "Accueil", url: "/",},
   {label: "Contact", url: "/contact",},
+  {label: "Vivre à Dommartin", url: "/dommartin/vivre-a-dommartin",},
   {label: "Historique", url: "/dommartin/historique",},
   {label: "Chapelle", url: "/dommartin/chapelle-niai-nion",},
   {label: "Croix", url: "/dommartin/croix-de-chemin",},
   {label: "Eglise", url: "/dommartin/eglise-saint-martin",},
-  {label: "Vivre à Dommartin", url: "/dommartin/vivre-a-dommartin",},
   {label: "Galerie photo", url: "/dommartin/galerie-photo",},
 ];
